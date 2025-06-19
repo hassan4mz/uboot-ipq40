499322a6b5f76c88cd4f67c7d76b16bcdef2354b
hassan4mz <57098971+hassan4mz@users.noreply.github.com>
2025-06-19 17:11:34 +0300

    Update ipq40xx_cdp.c

--- uboot/board/qcom/ipq40xx_cdp/ipq40xx_cdp.c
+++ uboot/board/qcom/ipq40xx_cdp/ipq40xx_cdp.c
@@ -461,9 +461,9 @@ void board_nand_init(void)
 			qpic_nand_init(&config);
 	}
 #endif
-	if ((gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK01_1_C2) ||
+	if ((gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK01_AP4220) ||
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK05_1_C1) ||
-		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK01_AP4220) ||
+		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK01_1_C2) ||
 		(gboard_param->machid == MACH_TYPE_IPQ40XX_AP_DK04_1_C5)) {
 		spi_nand_init();
 	}
@@ -586,14 +586,14 @@ static void ipq40xx_edma_common_init(void)
 void light_all_led(unsigned int machid)
 {
 	switch (machid) {
-	case MACH_TYPE_IPQ40XX_AP_DK01_1_C2:
+	case MACH_TYPE_IPQ40XX_AP_DK01_AP4220:
 		mdelay(1);
 		writel(GPIO_OUT, GPIO_IN_OUT_ADDR(GPIO_AP1300_POWER_LED));
 		mdelay(1);
 		writel(GPIO_OUT, GPIO_IN_OUT_ADDR(GPIO_AP1300_INET_LED));
 		mdelay(1);
 		break;
-	case MACH_TYPE_IPQ40XX_AP_DK01_AP4220:
+	case MACH_TYPE_IPQ40XX_AP_DK01_1_C2:
 		mdelay(1);
 		writel(GPIO_OUT, GPIO_IN_OUT_ADDR(GPIO_AP4220_POWER_LED));
 		mdelay(1);
@@ -652,8 +652,8 @@ int board_eth_init(bd_t *bis)
 	light_all_led(gboard_param->machid);
 	switch (gboard_param->machid) {
 	case MACH_TYPE_IPQ40XX_AP_DK01_1_S1:
-	case MACH_TYPE_IPQ40XX_AP_DK01_1_C2:
 	case MACH_TYPE_IPQ40XX_AP_DK01_AP4220:
+	case MACH_TYPE_IPQ40XX_AP_DK01_1_C2:
 	case MACH_TYPE_IPQ40XX_AP_DK05_1_C1:
 		mdelay(1);
 		writel(GPIO_OUT, GPIO_IN_OUT_ADDR(62));
